b3e577584b96bc672d1dfdd99dd8ca89d297ef7e
Ray Millward <Raymond.Millward@tessella.com>
2019-03-25 13:41:40 +0000

    Hooked in extra webpack config to make this a plugin

---
+++ webpack.config.extend.js
@@ -0,0 +1,38 @@
+/**
+ * Project's customized Webpack Configuration Extension
+ * ----------------------------------------------------
+ *
+ * this file is heavily inspired by `react-app-rewired` mechanism.
+ *
+ * it simply gives you the chance to hook into the default Webpack
+ * configuration as it is provided by `create-react-app`, and to
+ * change it so to match your project's needs.
+ *
+ * If you want to check out the default values look into:
+ * `./node_modules/marcopeg-react-scripts/config/webpack.config.${env}.js`
+ *
+ */
+
+module.exports = (webpackConfig, env, { paths }) => {
+  // here you can extend your webpackConfig at will
+
+  console.log(webpackConfig)
+
+  if (env == "production") {
+      webpackConfig.output.library = "demo_plugin"
+      webpackConfig.output.libraryTarget = "window"
+
+      webpackConfig.output.filename = '[name].js'
+      webpackConfig.output.chunkFilename = '[name].chunk.js'
+
+      delete webpackConfig.optimization.splitChunks
+      webpackConfig.optimization.runtimeChunk = false
+
+      // webpackConfig.externals = {
+      //     'react': 'React', // Case matters here 
+      //     'react-dom' : 'ReactDOM', // Case matters here 
+      // }
+  }
+
+  return webpackConfig
+}
