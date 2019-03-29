420d0cf11357031c10c15cd28888154080460b78
Louise Davies <louise.davies@stfc.ac.uk>
2019-03-29 14:31:32 +0000

    Implement componentDidCatch to log error and display fallback UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as log from 'loglevel';
 
-class App extends Component {
+class App extends Component<{}, { hasError: boolean }> {
+  public constructor(props: {}) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  public componentDidCatch(error: Error | null): void {
+    this.setState({ hasError: true });
+    log.error(`demo_plugin failed with error: ${error}`);
+  }
+
   public render(): React.ReactNode {
+    if (this.state.hasError) {
+      return (
+        <div className="error">
+          <div
+            style={{
+              padding: 20,
+              background: 'red',
+              color: 'white',
+              margin: 5,
+            }}
+          >
+            Something went wrong...
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="App">
         <div
